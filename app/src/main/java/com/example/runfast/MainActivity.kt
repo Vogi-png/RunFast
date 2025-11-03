66773c8ffe396ed27a632fa2bc7d5fd8bb72fcc8
Giovanna Leal <Work1now@myyahoo.com>
2025-11-03 16:14:44 -0300

    Initial commit

---
+++ app/src/main/java/com/example/runfast/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.runfast
+
+import android.app.Fragment
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.runfast.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId){
+                R.id.home -> replaceFragment(Home())
+                R.id.objective -> replaceFragment(Objective())
+                R.id.profile -> replaceFragment(Profile())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment (fragment : androidx.fragment.app.Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+}
